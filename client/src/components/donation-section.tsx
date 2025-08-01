2f39a971ddc3bef1f9b2fea9f93f6c7982bc2db8
Abhinab Sukla <137639013+Abhinab-123@users.noreply.github.com>
2025-08-01 13:16:25 +0530

    Update donation-section.tsx

--- client/src/components/donation-section.tsx
+++ client/src/components/donation-section.tsx
@@ -33,7 +33,7 @@ export default function DonationSection() {
                   </div>
                 </div>
                 <p className="text-sm text-gray-500 mt-4">
-                  UPI ID:8260978544@ibl
+                  UPI ID: 8260978544@ibl<br>Rudra Prasad Guru</br>
                 </p>
               </CardContent>
             </Card>
